62d0e5f54891b82ff1c0cf7ae57ebac057e177d2
ThibaultClerc <thibault.clerc2@gmail.com>
2020-10-27 11:37:11 +0100

    first commit app ready

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
